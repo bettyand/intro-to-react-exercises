d8c0f8a0283df6b911dad2fd2182c010e7e630f0
Betty Anderson <101119141+bettyand@users.noreply.github.com>
2022-04-14 16:29:21 -0400

    finished assignment

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+
+const Header = (props) => {
+  return (
+  <div>
+    <h1>{props.course}</h1>
+  </div>
+  )}
+
+const Part = (props) => {
+  return (
+    <div>
+      <p>{props.part} {props.exercises}</p>
+    </div>
+  )}
+
+  const Content = (props) => {
+    return (
+      <div>
+        <Part part={props.p1} exercises={props.e1} />
+        <Part part={props.p2} exercises={props.e2} />
+        <Part part={props.p3} exercises={props.e3} />
+      </div>
+  )}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>Number of exercises {props.ex1 + props.ex2 + props.ex3}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content p1={part1} e1={exercises1} p2={part2} e2={exercises2} p3={part3} e3={exercises3} />
+      <Total ex1={exercises1} ex2={exercises2} ex3={exercises3} />
+    </div>
+  )
+}
+
+export default App
